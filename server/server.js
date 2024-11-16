242a7fbc7cfd8401ab08963d6f8a1999d81ba714
Shreyansh Jain <147804449+sjain3082@users.noreply.github.com>
2024-11-16 11:05:43 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,40 @@
+// const http = require('http'),
+//       path = require('path'),
+//       express = require('express'),
+//       handlebars = require('express-handlebars'),
+//       socket = require('socket.io');
+
+// const config = require('../config');
+
+// const myIo = require('./sockets/io'),
+//       routes = require('./routes/routes');
+
+// const app = express(),
+//       server = http.Server(app),
+//       io = socket(server);
+
+// server.listen(config.port);
+
+// games = {};
+
+// myIo(io);
+
+// console.log(`Server listening on port ${config.port}`);
+
+// const Handlebars = handlebars.create({
+//   extname: '.html', 
+//   partialsDir: path.join(__dirname, '..', 'front', 'views', 'partials'), 
+//   defaultLayout: false,
+//   helpers: {}
+// });
+// app.engine('html', Handlebars.engine);
+// app.set('view engine', 'html');
+// app.set('views', path.join(__dirname, '..', 'front', 'views'));
+// app.use('/public', express.static(path.join(__dirname, '..', 'front', 'public')));
+
+// routes(app);
+
+
 const http = require('http'),
       path = require('path'),
       express = require('express'),
@@ -5,7 +42,6 @@ const http = require('http'),
       socket = require('socket.io');
 
 const config = require('../config');
-
 const myIo = require('./sockets/io'),
       routes = require('./routes/routes');
 
@@ -13,13 +49,25 @@ const app = express(),
       server = http.Server(app),
       io = socket(server);
 
-server.listen(config.port);
+let port = config.port;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+}).on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Port ${port} is already in use. Trying another port...`);
+    server.listen(0, () => {
+      const newPort = server.address().port;
+      console.log(`Server is now listening on port ${newPort}`);
+    });
+  } else {
+    console.error('Server error:', err);
+  }
+});
 
 games = {};
 
 myIo(io);
-
-console.log(`Server listening on port ${config.port}`);
 
 const Handlebars = handlebars.create({
   extname: '.html', 
